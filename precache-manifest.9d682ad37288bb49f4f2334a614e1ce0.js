03c07dcbf06e5485244f866e06e56aeb4ac33057
eliasDsramirez <elias.bld@hotmail.com>
2020-01-16 13:18:39 -0300

    Updates

---
+++ precache-manifest.9d682ad37288bb49f4f2334a614e1ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968a5a3312ea2ab91ba63d8f654454fa",
+    "url": "/roledex-monsters/index.html"
+  },
+  {
+    "revision": "6f88927ab639271913f9",
+    "url": "/roledex-monsters/static/css/main.993a6b36.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/roledex-monsters/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/roledex-monsters/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f88927ab639271913f9",
+    "url": "/roledex-monsters/static/js/main.8b2af3c0.chunk.js"
+  },
+  {
+    "revision": "d21a591368e0dc92854f",
+    "url": "/roledex-monsters/static/js/runtime-main.93c2d0f8.js"
+  }
+]);
